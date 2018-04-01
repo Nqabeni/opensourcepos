e52c8aa1ee9f64c5fc31c9c51cadfedbc5ce7fd2
jekkos <jeroen.peelaerts@gmail.com>
2018-04-01 16:24:10 +0200

    Migration refactor: move out common code (#1440)

--- application/migrations/20180220100000_attributes.php
+++ application/migrations/20180220100000_attributes.php
@@ -9,32 +9,7 @@ class Migration_Attributes extends CI_Migration
 
 	public function up()
 	{
-		error_log('Migrating to Attributes');
-
-		$sql = file_get_contents(APPPATH . 'migrations/sqlscripts/attributes.sql');
-
-		/*
-		CI migration only allows you to run one statement at a time.
-		This small script splits the statements allowing you to run them all in one go.
-		*/
-
-		$sqls = explode(';', $sql);
-		array_pop($sqls);
-
-		foreach($sqls as $statement)
-		{
-			$statement = $statement . ';';
-
-			if(!$this->db->simple_query($statement))
-			{
-				foreach($this->db->error() as $error)
-				{
-					error_log('error: ' . $error);
-				}
-			}
-		}
-
-		error_log('Migrated to Attributes');
+		execute_script(APPPATH . 'migrations/sqlscripts/attributes.sql');
 	}
 
 	public function down()
